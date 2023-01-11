bd5b3dfd22292b164266ae90c56ef92ac3c93d23
ericcarino <eric@nefeli.io>
2023-01-10 20:06:05 -0800

    2 files modified

--- src/app/create-group-dialog/create-group-dialog.component.ts
+++ src/app/create-group-dialog/create-group-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
-import { Application, Applications, Services, VirtualMachine, VirtualMachineGroup, VirtualMachineGroups, VirtualMachines } from '../data';
+import { Application, ApplicationKey, Applications, ServiceAccessPolicies, Services, VirtualMachine, VirtualMachineGroup, VirtualMachineGroups, VirtualMachines } from '../data';
 
 @Component({
   selector: 'app-create-group-dialog',
@@ -8,6 +8,12 @@ import { Application, Applications, Services, VirtualMachine, VirtualMachineGrou
   styleUrls: ['./create-group-dialog.component.css']
 })
 export class CreateGroupDialogComponent implements OnInit {
+  private _serviceId = Math.random();
+  serviceName?: string;
+  servingGroupName?: string;
+  consumingGroupName?: string;
+  applicationName?: string;
+
 
   policyForm = this._fb.group({
     name: [''],
@@ -38,16 +44,29 @@ export class CreateGroupDialogComponent implements OnInit {
 
   public createService() {
     const serviceName = this.policyForm.get('serviceName').value as unknown as string;
-    const application = this.policyForm.get('application').value as unknown as string;
+    const application = this.policyForm.get('application').value as unknown as ApplicationKey;
     const servingGroup = this.policyForm.get('servingGroup').value as unknown as VirtualMachineGroup;
 
+    this.serviceName = serviceName;
+    this.servingGroupName = servingGroup.name;
+
     Services.push({
-      id: Math.random(),
-      application: application.,
+      id: this._serviceId,
+      application: application,
       groupId: servingGroup.id
     });
+  }
 
-    debugger
+  public createPolicy() {
+    const consumingGroup = this.policyForm.get('consumingGroup').value as unknown as VirtualMachineGroup;
+
+    this.consumingGroupName = consumingGroup.name;
+
+    ServiceAccessPolicies.push({
+      id: Math.random(),
+      serviceId: this._serviceId,
+      groupId: consumingGroup.id
+    });
   }
 
   public get virtualMachines(): VirtualMachine[] {
